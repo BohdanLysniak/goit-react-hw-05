09a9b08ff16bf381a7e752dfba67c98ee4cca743
BohdanLysniak <marttelo@ukr.net>
2024-06-06 20:08:35 +0300

    task in work

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -3,15 +3,21 @@ import { useEffect, useState } from "react";
 import { getMoviesByQuery } from "../../serviceAPI/tmdbApi";
 import SearchMovies from "../../components/SearchMovies/SearchMovies";
 import MovieList from "../../components/MovieList/MovieList";
+import { useSearchParams } from "react-router-dom";
 
 export default function MoviesPage() {
   const [movies, setMovies] = useState([]);
-  const [query, setQuery] = useState("");
-  // const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const movieTitle = searchParams.get("movieTitle") && "";
+
+  const changeMovieTitle = newMovieTitle => {
+    searchParams.set("movieTitle", newMovieTitle);
+    setSearchParams(searchParams);
+  };
 
   const handleSubmit = event => {
     event.preventDefault();
-    setQuery("");
+    setMovies("");
   };
 
   console.log(movies);
@@ -19,17 +25,22 @@ export default function MoviesPage() {
   useEffect(() => {
     async function fetchMoviesByQuery() {
       try {
-        const { results } = await getMoviesByQuery();
+        const { results } = await getMoviesByQuery(searchParams);
         setMovies(results);
       } catch (error) {
         console.log(error);
       }
     }
     fetchMoviesByQuery();
-  }, []);
+  }, [searchParams]);
+
   return (
     <div>
-      <SearchMovies value={query} onChange={setQuery} onSubmit={handleSubmit} />
+      <SearchMovies
+        value={movieTitle}
+        onChange={changeMovieTitle}
+        onSubmit={handleSubmit}
+      />
       {movies.length > 0 && <MovieList moviesList={movies} />}
     </div>
   );
